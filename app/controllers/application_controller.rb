32adce7560d60f9ef3b16bfbb330911d5d91d68e
Philip Levy <philipdlevy@gmail.com>
2022-06-21 11:13:42 -0600

    added more routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
@@ -20,6 +20,37 @@ class ApplicationController < Sinatra::Base
     books.to_json
   end
 
+  post "/books" do 
+    new_book = Book.create(
+      title: params[:title],
+      author_name: params[:author_name],
+      description: params[:description],
+      price: params[:price],
+      pages: params[:pages],
+      author_id: params[:author_id]
+    )
+    new_book.to_json
+  end
+
+  patch "/books/:id" do
+    book = Book.find(params[:id])
+    book.update(
+      title: params[:title],
+      author_name: params[:author_name],
+      description: params[:description],
+      price: params[:price],
+      pages: params[:pages],
+      )
+      book.to_json
+  end
+
+  delete "/books/:id" do
+    book = Book.find(params[:id])
+    book.destroy
+    book.to_json
+  end
+
+
 
 # Author routes
   get "/authors" do 
@@ -29,6 +60,27 @@ class ApplicationController < Sinatra::Base
 
   get "/authors/:id" do 
     authors = Author.find(params[:id])
+    authors.to_json
+  end
+
+  post "/authors" do 
+    new_author = Author.create(
+      name: params[:name]
+    )
+    new_author.to_json
+  end
+
+  patch "/authors/:id" do
+    author = Author.find(params[:id])
+    author.update(
+      name: params[:name],
+      )
+      author.to_json
+  end
+
+  delete "/authors/:id" do
+    authors = Author.find(params[:id])
+    authors.destroy
     authors.to_json
   end
 
